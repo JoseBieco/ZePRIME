2f9c262c9a55f224d5a92956bb62a004c1719a0b
JoseBieco <jo_bieco@hotmail.com>
2020-07-28 22:31:23 -0300

    Footer alteration

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,11 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        <a href="https://twitter.com/_Ze13">
+          José Eduardo Saroba Bieco
         </a>
       </p>
     </FooterBase>
