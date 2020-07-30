8cee238e422f099394a3fbd4a6c0a1e56c4b8052
JoseBieco <jo_bieco@hotmail.com>
2020-07-30 18:50:25 -0300

    Aula 3.allFixed

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../assets/img/Logo.png';
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/img/logo.png';
 import './Menu.css';
 import Button from '../Button';
-// import ButtonLink from './components/ButtonLink';
+
+//import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-  return (
-    <nav className="Menu">
-      <Link to="/">
-        <img className="Logo" src={Logo} alt="AluraFlix logo" />
-      </Link>
+    return (
+        <header>
+            <nav className="Menu">
+                <Link to="/">
+                    <img className="Logo" src={Logo} alt="ZePRIME logo"></img>
+                </Link>
 
-      <Button as={Link} className="ButtonLink" to="/cadastro/video">
-        Novo vídeo
-      </Button>
-    </nav>
-  );
+                <Button as={Link} className="ButtonLink" to="/cadastro/video">
+                    Novo vídeo
+                </Button>
+            </nav>
+        </header>
+    );
 }
 
 export default Menu;
--- src/index.js
+++ src/index.js
@@ -9,14 +9,12 @@ import CadastroCategoria from './pages/cadastro/Categoria';
 
 const Pagina404 = () => (<div>Página 404</div>) 
 
-const Pagina404 = () => (<div>Página 404</div>)
-
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/cadastro/Categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
